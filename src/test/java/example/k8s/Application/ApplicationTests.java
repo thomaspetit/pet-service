47df9191c60698465d4c23bfd76879f4d49a73b0
Thomas Petit <thomas.marc.petit@gmail.com>
2019-02-21 20:33:37 +0100

    initial commit

---
+++ src/test/java/example/k8s/Application/ApplicationTests.java
@@ -0,0 +1,17 @@
+package example.k8s.Application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
